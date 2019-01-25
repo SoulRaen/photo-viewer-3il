233ddfe58c4dca44f53f9a903f8bf98448436cbd
Maxime Defrances <maxime.defrances@hotmail.fr>
2019-01-25 10:40:03 +0100

    Fin traduction

--- espace-pro.php
+++ espace-pro.php
@@ -8,17 +8,12 @@
 <?= getMenu("Espace Pro") ?>
         <section>
             <form class="form-signin">
-                <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+                <h1 class="h3 main-title">Page de connexion</h1>
                 <label for="inputEmail" class="sr-only">Email address</label>
                 <input type="email" id="inputEmail" class="form-control" placeholder="Adresse mail" required autofocus>
                 <label for="inputPassword" class="sr-only">Password</label>
                 <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
-                <!--div class="checkbox mb-3">
-                    <label>
-                        <input type="checkbox" value="remember-me"> Remember me
-                    </label>
-                </div-->
-                <button class="submit-btn" type="submit">Sign in</button>
+                <button class="submit-btn" type="submit">Se connecter</button>
             </form>
         </section>
     </body>
