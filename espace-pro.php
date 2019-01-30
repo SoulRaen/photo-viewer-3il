59105e95fa8761b1c01bf282c557c83699fb4447
Maxime Defrances <maxime.defrances@hotmail.fr>
2019-01-30 18:23:23 +0100

    correction après push foireux d'olivier :)

--- espace-pro.php
+++ espace-pro.php
@@ -6,12 +6,12 @@
 <?= getHeader() ?>
 <?= getMenu("Espace Pro") ?>
         <section>
-            <form class="form-signin" method="post" action="./php/connexion.php">
+            <div class="form-signin" method="post" action="./php/connexion.php">
                 <h1 class="h3 signin-title">Page de connexion</h1>
-                <input type="email" name="user_email" id="inputEmail" class="form-control" placeholder="Adresse mail" required autofocus>
-                <input type="password" name="user_pw" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
-                <button class="submit-btn" type="submit">Se connecter</button>
-            </form>
+                <input type="login" id="inputLogin" class="form-control" placeholder="Login" required autofocus>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
+                <button class="submit-btn">Se connecter</button>
+            </div>
         </section>
 <?= getJQuery() ?>
     </body>
--- php/connexion.php
+++ php/connexion.php
@@ -7,20 +7,12 @@ $dbname="siteweb";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-<<<<<<< HEAD
     /* Paramétrage requête */
     $stmt = $conn->prepare("SELECT uID,email,mdp,nom,prenom FROM users WHERE login='".$_POST["user_login"]."'");
     /* Execution requête */
-=======
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "user email : ".$_POST["user_email"];
-    $stmt = $conn->prepare("SELECT uID,login,mdp FROM users WHERE email='".$_POST["user_email"]."'"); 
-    echo "<BR>user pw : ".$_POST["user_pw"];
->>>>>>> f2446b06b6ae00822972a595cdc52db7b77e14cd
     $stmt->execute();
     echo "<BR>user pw : ".$_POST["user_pw"];
     /* Récupération de toutes les lignes d'un jeu de résultats */
-    //print("Récupération de toutes les lignes d'un jeu de résultats :\n");
     $results = $stmt->fetchAll();
     /* Si plusieurs résultats, erreur */
     if (isset($results[1])){
