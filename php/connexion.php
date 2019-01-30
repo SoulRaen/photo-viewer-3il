7c9a096651bcf49d24f1828d996fc560c9ca4cd2
Maxime Defrances <maxime.defrances@hotmail.fr>
2019-01-30 17:12:13 +0100

    système de connexion

--- php/connexion.php
+++ php/connexion.php
@@ -6,27 +6,29 @@ $password = "";
 $dbname="siteweb";
 
 try {
+    /* Paramétrage connexion */
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "user email : ".$_POST["user_email"];
-    $stmt = $conn->prepare("SELECT uID,login,mdp FROM users WHERE email='".$_POST["user_email"]."'"); 
-    echo "<BR>user pw : ".$_POST["user_pw"];
+    /* Paramétrage requête */
+    $stmt = $conn->prepare("SELECT uID,email,mdp FROM users WHERE login='".$_POST["user_login"]."'");
+    /* Execution requête */
     $stmt->execute();
-    echo "<BR>user pw : ".$_POST["user_pw"];
-    /* Récupération de toutes les lignes d'un jeu de résultats */
-    //print("Récupération de toutes les lignes d'un jeu de résultats :\n");
+    /* Traitement des infos */
     $results = $stmt->fetchAll();
-    echo "<BR>user pw : ".$_POST["user_pw"];
-    echo "<br>uID : ".$results[0]["uID"];
-    echo "<br>login : ".$results[0]["login"];
-    echo "<br>mdp : ".$results[0]["mdp"];
-    /*foreach ($results as $i => $result) {
-        //echo "\$results[$k] => $v.\n";
-        foreach ($result as $j => $value) {
-            echo "<br>value : ".$value;
+    /* Si plusieurs résultats, erreur */
+    if (isset($results[1])){
+        echo "multiple results";
+    }else if(isset($results[0])){       /* Si un résultat présent, traiter */
+        if($results[0]["mdp"]==$_POST["user_pw"]){
+            echo "connect ok";
+            session_start();
+            $_SESSION['connected_login']=$_POST["user_login"];
+        }else{
+            echo "wrong pw";
         }
-    }*/
-    //print_r($result[0]["contenu"]);
+    }else {     /* Si 0 résultat */
+        echo "no result";
+    }
 }
 catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
