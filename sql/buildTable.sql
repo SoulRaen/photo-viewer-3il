cc817b1d4603929911c08cc7c3aa4c4c8a3011f3
Maxime Defrances <34211283+UBowt571@users.noreply.github.com>
2019-02-01 15:57:23 +0100

    Update buildTable.sql
    ajout un utilisateur

--- sql/buildTable.sql
+++ sql/buildTable.sql
@@ -1,1 +1,2 @@
 CREATE TABLE `siteweb`.`users` ( `uID` INT NOT NULL AUTO_INCREMENT , `login` TEXT NOT NULL , `email` TEXT NOT NULL , `mdp` TEXT NOT NULL , `nom` TEXT NOT NULL , `prenom` TEXT NOT NULL , PRIMARY KEY (`uID`)) ENGINE = InnoDB;
+INSERT INTO `users` (`uID`, `login`, `email`, `mdp`, `nom`, `prenom`) VALUES (NULL, 'm.defrances', 'maxime.defrances', '123456', 'defrances', 'maxime');
