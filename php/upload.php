3e9342beb59123da9396222945eb033a21589267
Maxime Defrances <maxime.defrances@hotmail.fr>
2019-02-06 20:12:49 +0100

    finition système de connexion

--- php/upload.php
+++ php/upload.php
@@ -1,33 +1,24 @@
 <?php
 
+header("Content-Type: application/json");
+
 // Check if image file is a actual image or fake image
-/*if(isset($_POST["submit"])) {
-    $file = $_FILES["fileToUpload"]["tmp_name"];
-    if (file_exists("C:/xampp/htdocs/photo-viewer-3il/img/".$_FILES["fileToUpload"]["name"])) {
-        echo "Sorry, file already exists.";
-    }
-    rename("$file", "C:/xampp/htdocs/photo-viewer-3il/img/".$_FILES["fileToUpload"]["name"]);
-}*/
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
-echo "<pre>";
-print_r($_FILES);
-echo "</pre>";
-
-echo getcwd();
-
-$target_dir = "../img";
-$target_file = $target_dir . basename($_FILES[0]["name"]);
+$target_dir = "../img/";
+$target_file = $target_dir . basename($_FILES["file"]["name"]);
+$source_file = $_FILES["file"]["tmp_name"];
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-echo "target file : ".$target_file;
 // Check if file already exists
-//if (file_exists($target_file)) {
-//    echo "fichier existe deja";
-//    $uploadOk = 0;
-//}else if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" && $imageFileType != "bmp" ) {
-//    echo "mauvais type image";
-//    $uploadOk = 0;
-//}
+if (file_exists($target_file)) {
+    $coderesultat = array("code resultat" => "fichier existe deja");
+    echo json_encode($coderesultat,JSON_FORCE_OBJECT);
+}else if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" && $imageFileType != "bmp" ) {
+    $coderesultat = array("code resultat" => "mauvais type image");
+    echo json_encode($coderesultat,JSON_FORCE_OBJECT);
+}else{
+    if(rename($source_file, $target_file)){
+        $coderesultat = array("code resultat" => "upload OK");
+        echo json_encode($coderesultat,JSON_FORCE_OBJECT);
+    }
+}
 ?>
