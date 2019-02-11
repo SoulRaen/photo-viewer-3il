0e778633647fa05ad514ca7a57eb86347385dfec
SoulRaen <34211185+SoulRaen@users.noreply.github.com>
2019-02-11 14:50:04 +0100

    fonction getContenu()

--- php/acces-contenu.php
+++ php/acces-contenu.php
@@ -5,20 +5,29 @@ const USERNAME = "root";
 const PASSWORD = "";
 const DATABASE ="siteweb";
 
-function getContenuAccueil() {
+function getContenu($page) {
     try {
         /* Paramétrage connexion */
-        $conn = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USERNAME, PASSWORD);
+        $conn = new PDO("mysql:host=".HOST.";dbname=".DATABASE.";charset=utf8", USERNAME, PASSWORD);
         /* Paramétrage requête */
-        $stmt = $conn->prepare("SELECT contenu FROM sections WHERE page_id = (SELECT uID FROM pages WHERE nom = 'index.php');");
+        $stmt = $conn->prepare("SELECT contenu FROM sections WHERE page_id = (SELECT uID FROM pages WHERE nom = :page) ORDER BY date_creation DESC;");
+        $stmt->bindValue(":page", $page, PDO::PARAM_STR);
         /* Execution requête */
         if ($stmt->execute()) {
-            /* Traitement des infos */
+            /* Récupération du contenu */
             $results = $stmt->fetchAll();
             if (!empty($results)) {
-                return $results[0]['contenu'];
+                $contenu = "";
+                foreach ($results as $ligne) {
+                    $contenu .= <<<EOT
+        <section>
+{$ligne['contenu']}
+        </section>
+EOT;
+                }
+                return $contenu;
             }
-            return "<p>Cette page est vide !</p>";
+            return "<section><p>Cette page est vide !</p><section>";
         } else { //erreur à l'exécution de la requête
             $erreur = $stmt->errorInfo();
             return "<span class=\"erreur-bdd\">Error: SQLSTATE[{$erreur[0]}] [{$erreur[1]}] " . utf8_encode($erreur[2]) . "</span>";
