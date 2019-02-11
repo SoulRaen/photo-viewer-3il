e3ff228ce648ff81d7c6295b73efcde8b44fa49d
Maxime Defrances <maxime.defrances@hotmail.fr>
2019-02-11 14:24:03 +0100

    avancement edit-accueil.php

--- edit-accueil.php
+++ edit-accueil.php
@@ -6,5 +6,14 @@
     <body>
 <?= getHeader() ?>
 <?= getMenu("Espace Pro") ?>
+        <section>
+            <h1 class="centered-title">Mise à jour du contenu de la page d'accueil</h1>
+            <textarea id ="edit-accueil"></textarea>
+            <div>
+                <button class="submit-btn" id ="send-accueil-info-btn">Annuler</button>
+                <button></button>
+            </div>
+        </section>
+        <?= getScriptsCommuns() ?>
     </body>
 </html>
