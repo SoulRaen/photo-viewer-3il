fa40493d622a4576cbb7aff1de15de2fa03e8ba6
Maxime Defrances <maxime.defrances@hotmail.fr>
2019-02-14 14:30:49 +0100

    maj edit accueil

--- edit-accueil.php
+++ edit-accueil.php
@@ -15,5 +15,48 @@
             </div>
         </section>
         <?= getScriptsCommuns() ?>
+        <script type="text/javascript">
+            window.onload=function(e){
+                var xmlhttp;
+                if(window.XMLHttpRequest){          /* Si XMLHttpRequest supporté */
+                    xmlhttp= new XMLHttpRequest();
+
+                    /* Paramètre le type de connexion + la destination + type de contenu + contenu */
+                    xmlhttp.open("GET","./php/connexion.php",true);
+                    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+                    xmlhttp.send("user_login="+login+"&user_pw="+mdp);
+                    /* Quand l'état change */
+                    xmlhttp.onreadystatechange = function (){
+                        /* Chargement de la réponse finie + status HTTP OK */
+                        if (xmlhttp.readyState ==4 && xmlhttp.status ==200){
+                            var jsonobj = JSON.parse(xmlhttp.responseText);
+
+                            /* Réaction à la réponse */
+                            switch (jsonobj["code resultat"]) {
+                                case "pas de resultats" :
+                                    alert("Mauvais login ! : Pas de résultat");
+                                    break;
+                                case "mauvais mdp" :
+                                    alert("Mauvais mot de passe !");
+                                    break;
+                                case "resultats mutiples" :
+                                    alert("Mauvais login ! : plusieurs résultat");
+                                    break;
+                                case "connexion OK" :
+                                    /* Empêche de se reconnecter une fois connecté */
+                                    nbconnect++;
+
+                                    alert("Connecté pour "+jsonobj["duree-session-min"]+" minute(s)");
+                                    
+                                    /* Rafraîchis la page */
+                                    window.location.replace("./espace-pro.php");
+                            }
+                        }
+                    }
+                }else{
+                    alert("Navigateur obsolète, veuillez le mettre à jour");
+                }
+            }
+        </script>
     </body>
 </html>
