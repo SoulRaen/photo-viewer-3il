2f4c45646ba7e56fe4aef452d30841d4d210734b
Maxime Defrances <maxime.defrances@hotmail.fr>
2019-02-14 14:32:44 +0100

    mini maj pour pull

--- edit-accueil.php
+++ edit-accueil.php
@@ -8,7 +8,7 @@
 <?= getMenu("Espace Pro") ?>
         <section>
             <h1 class="centered-title">Mise à jour du contenu de la page d'accueil</h1>
-            <textarea id ="edit-accueil"></textarea>
+            <textarea id ="edit-accueil"><?=?></textarea>
             <div>
                 <button class="submit-btn send-accueil-info-btn align-left"><img class ="img-in-text" src="https://storage.needpix.com/rsynced_images/cross-mark-304374_1280.png"> Annuler</button>
                 <button class="submit-btn send-accueil-info-btn align-right"><img class ="img-in-text" src="http://www.clker.com/cliparts/2/k/n/l/C/Q/transparent-green-checkmark-md.png"> Accepter</button>
