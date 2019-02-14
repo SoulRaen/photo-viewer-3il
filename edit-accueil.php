5253e55bf58472d6105ed239de722fccf93c827a
Maxime Defrances <maxime.defrances@hotmail.fr>
2019-02-14 17:00:22 +0100

    MàJ images

--- edit-accueil.php
+++ edit-accueil.php
@@ -12,12 +12,26 @@
 <?= getMenu("Espace Pro") ?>
         <section>
             <h2 class="centered-title">Édition de la page Accueil</h2>
-            <textarea spellcheck="false" class ="edit"><?= getContenu("index.php",true)[0]["contenu"] ?></textarea>
+            <textarea spellcheck="false" class ="edit" id ="text-window"><?= getContenu("index.php",true)[0]["contenu"] ?></textarea>
             <div>
-                <button class="submit-btn send-info-btn align-left"><img class ="img-in-text" src="https://storage.needpix.com/rsynced_images/cross-mark-304374_1280.png"> Annuler</button>
-                <button class="submit-btn send-info-btn align-right"><img class ="img-in-text" src="http://www.clker.com/cliparts/2/k/n/l/C/Q/transparent-green-checkmark-md.png"> Accepter</button>
+                <button class="submit-btn send-info-btn align-left" onclick="abortChanges()"><img class ="img-in-text" src="assets/red-cross-error.png"> Annuler</button>
+                <button class="submit-btn send-info-btn align-right" onclick="updateContent()"><img class ="img-in-text" src="assets/green-check-mark.png"> Accepter</button>
             </div>
         </section>
 <?= getScriptsCommuns() ?>
+        <script>
+            function abortChanges(){
+                /* Redirige vers l'accueil */
+                window.location.replace("/");
+            }
+
+            function updateContent(){
+                /* */
+                alert("ok");
+                var content = document.getElementById("text-window").value;
+                console.log(content);
+            }
+
+        </script>
     </body>
 </html>
