f8a1c860ceaabfe43ae66a73fdaf7231b098cf5e
SoulRaen <34211185+SoulRaen@users.noreply.github.com>
2019-02-18 16:48:30 +0100

    ajout suppression d'image

--- edit-photos.php
+++ edit-photos.php
@@ -22,8 +22,8 @@
             <h3 class="centered-title">Modification du texte</h3>
             <textarea spellcheck="false" class="edit"><?= $results[0]['contenu'] ?? "" ?></textarea>
             <div>
-                <button class="submit-btn send-info-btn align-left"><img class ="img-in-text" src="https://storage.needpix.com/rsynced_images/cross-mark-304374_1280.png"> Annuler</button>
-                <button class="submit-btn send-info-btn align-right"><img class ="img-in-text" src="http://www.clker.com/cliparts/2/k/n/l/C/Q/transparent-green-checkmark-md.png"> Accepter</button>
+                <button class="submit-btn send-info-btn align-left"><img class ="img-in-text" src="assets/red-cross-error.png"> Annuler</button>
+                <button class="submit-btn send-info-btn align-right"><img class ="img-in-text" src="assets/green-check-mark.png"> Accepter</button>
             </div>
         </section>
         <!-- PARTIE AJOUT -->
@@ -37,8 +37,16 @@
         <!-- PARTIE SUPPRESSION -->
         <section id="suppr-photos" style="display: none">
             <h3 class="centered-title">Suppression d'images</h3>
+            <button onclick="getImages()">Charger les images</button>
+            <div id="carrousel"></div>
+            <!-- style dans l'élément pour pouvoir utiliser les fonctions jQuery hide() et show()-->
+            <button class="bouton-carrousel" id="bouton-carrousel-gauche" style="display:none">&lt;</button>
+            <button class="bouton-carrousel" id="bouton-carrousel-droite" style="display:none">&gt;</button>
+            <div id="compteur-images" style="display:none"></div>
+            <input type="button" onclick="supprimerImage()" value="Supprimer" class="submit-btn"/>
         </section>
 <?= getScriptsCommuns() ?>
+        <script src="./js/carrousel.js"></script>
         <script src="./js/upload.js"></script>
         <script type="text/javascript">
             $("input[name=mode]").on("change", function () {
