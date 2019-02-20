ee768bc633352fb5015b1886d376ae0a20d8b77d
SoulRaen <34211185+SoulRaen@users.noreply.github.com>
2019-02-20 11:57:03 +0100

    controle des acces

--- php/edit-pages/images.php
+++ php/edit-pages/images.php
@@ -1,5 +1,8 @@
 <?php
 header("Content-Type: application/json");
+if (empty($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest' ) {
+    header('Location: ../../index.php'); 
+}
 $files = array_diff(scandir("../../img"), [".","..","Thumbs.db"]);
 sort($files);
 echo json_encode($files);
